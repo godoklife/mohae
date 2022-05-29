eb21047f373c7df7651e5d6d40ac7ce0cc583edb
XPS-15 <XPS-15@XPS15.mshome.net>
2022-05-29 19:31:53 +0900

    SQL문 수정

--- mohae/src/main/webapp/sql/plan.sql
+++ mohae/src/main/webapp/sql/plan.sql
@@ -1,6 +1,8 @@
 create table `mohaemohae`.`plan`(			-- [ 아직 정확히 어떤 데이타가 필요한지 몰라서 미완성 상태. 추후 보충 요망 ]
-	planno int unsigned primary key auto_increment not null,
-    planpeoplecount int1 unsigned not null,
-    memberno int unsigned,					-- [ FK ]
-    foreign key(memberno) references member(memberno) on update cascade	-- 모집글 작성자의 정보 참조용
+	planno int unsigned primary key auto_increment not null,	-- PK
+    planlatitude decimal(16,14),		-- 위도값, 예시) 33.45067375096625, null이 될 수 있음.
+    planlongitude decimal(16,13),		-- 경도값, 예시) 126.5706721005115, null이 될 수 있음.
+    planpeoplenumber int1 not null,		-- 인원수
+    boardno int unsigned not null, 		-- [ FK ] 게시판 번호
+    foreign key(boardno) references board(boardno) on delete cascade	-- [ 여행 모집 게시글이 사라지면 같이 지워짐 ] 
 );
